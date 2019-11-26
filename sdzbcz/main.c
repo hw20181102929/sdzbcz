dd040ba437288a3fa131dd51aab4850d24efb655
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-11-26 17:04:38 +0800

    Initial Commit

---
+++ sdzbcz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sdzbcz
+//
+//  Created by s20181102929 on 2019/11/26.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
